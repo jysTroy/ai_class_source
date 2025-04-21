ded3f7c01434ae284412c1e527598da181dbbb10
yesung <jys_troy@naver.com>
2025-04-21 18:18:00 +0900

    .

---
+++ 04_자바기초/source/day02/src/exam02/Ex05.java
@@ -0,0 +1,10 @@
+package exam02;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        // 배열의 선언과 동시에 초기화
+//        int[] nums = new int[] {10, 20, 30, 40};
+        int[] nums = {10, 20, 30, 40};
+        System.out.printf("nums[0]:%d, nums[1]:%d, nums[2]:%d, nums[3]:%d%n", nums[0], nums[1], nums[2], nums[3]);
+    }
+}
---
+++ 04_자바기초/source/day02/src/exam03/Ex02.java
@@ -0,0 +1,22 @@
+package exam03;
+
+public class Ex02 {
+    /**
+     * main() 호출 -> add(10, 20) 호출
+     * @param args
+     */
+
+    // 메인 함수 - 모든 프로그램의 시작 함수
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result2 = add(num1,num2);
+//        System.out.println(result2);
+    }
+
+    static int add(int num1, int num2) {
+        int result = num1 + num2;
+
+        return result;
+    }
+}
