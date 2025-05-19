a245237641480b4de0d07da158f3e2af878cf2f6
yesung <jys_troy@naver.com>
2025-05-19 11:16:01 +0900

    .

---
+++ 06_스프링_기초/source/springex4/src/main/java/org/koreait/global/configs/MvcConfig.java
@@ -0,0 +1,29 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.koreait")
+@Import(ControllerConfig.class)
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/templates/", ".jsp");
+    }
+}
