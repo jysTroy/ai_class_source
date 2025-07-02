8bc00238a451fda3e813427b3ee7dc4ec70b7e2c
yesung <jys_troy@naver.com>
2025-07-02 14:59:08 +0900

    add

--- hangingBr/index.js
+++ hangingBr/index.js
@@ -1,14 +1,3 @@
 document.body.style.transform = "rotate(4deg)";
 
-const emo = document.createElement("div");
-emo.textContent = "🦖";
-emo.style.position = "fixed";
-emo.style.fontSize = "2rem";
-emo.style.pointerEvents = "none";
-emo.style.zIndex = 9999;
-document.body.appendChild(emo);
 
-document.addEventListener("mousemove", (e) => {
-  emo.style.left = `${e.pageX + 10}px`;
-  emo.style.top = `${e.pageY + 10}px`;
-});
