c40e754361e715d277432ebec2fa87e335b92b3b
yesung <jys_troy@naver.com>
2025-07-10 16:22:29 +0900

    .

--- 11_금융_OPENAPI_활용/source/exam01/src/main/java/org/koreait/member/controllers/RequestJoin.java
+++ 11_금융_OPENAPI_활용/source/exam01/src/main/java/org/koreait/member/controllers/RequestJoin.java
@@ -1,10 +1,18 @@
 package org.koreait.member.controllers;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RequestJoin {
+    @Email(message = "이메일 형식 아님")
+    @NotBlank(message = "이메일 쓰셈")
     private String email;
+
+    @NotBlank(message = "비밀번호 쓰셈")
     private String password;
+
+    @NotBlank(message = "회원명 쓰셈")
     private String name;
 }
