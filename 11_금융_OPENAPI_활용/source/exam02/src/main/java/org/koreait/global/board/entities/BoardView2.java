8d814531921e3ffb19406284675f47375cc4b362
yesung <jys_troy@naver.com>
2025-07-16 15:58:24 +0900

    .

---
+++ 11_금융_OPENAPI_활용/source/exam02/src/main/java/org/koreait/global/board/entities/BoardView2.java
@@ -0,0 +1,17 @@
+package org.koreait.global.board.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import lombok.Data;
+
+@Data
+@Entity
+@IdClass(BoardView2.class)
+public class BoardView2 {
+    @Id
+    private Long seq;
+
+    @Id
+    private int uid;
+}
--- 11_금융_OPENAPI_활용/source/exam02/src/main/java/org/koreait/member/constants/Authority.java
+++ 11_금융_OPENAPI_활용/source/exam02/src/main/java/org/koreait/member/constants/Authority.java
@@ -1,7 +1,7 @@
 package org.koreait.member.constants;
 
 public enum Authority {
-    Member, // 일반회원
+    MEMBER, // 일반회원
     MANAGER, // 부관리자
     ADMIN // 관리자
 
